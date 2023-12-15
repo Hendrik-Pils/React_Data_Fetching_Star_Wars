72a9988304abdb27348b66d55c05ababf5975dc9
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-15 12:04:32 +0100

    finish challenge

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
+  const URL = "https://swapi.dev/api/people/";
+
+  const fetcher = async (URL) => {
+    const res = await fetch(URL);
+
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          fetcher,
+          refreshInterval: 5000,
+        }}
+      >
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
---
+++ pages/characters/[id].js
@@ -0,0 +1,33 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`
+  );
+
+  if (error)
+    return (
+      <div>
+        {error.info} - {error.status}
+      </div>
+    );
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
